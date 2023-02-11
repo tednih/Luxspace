e2c84c247525d0956a934545b66e76bca1904279
tednih <122004183+tednih@users.noreply.github.com>
2023-02-11 17:46:12 +0800

    Update products detail pages -fix slider preview -fix main content -fix Suggestion section -add to chart animation -add skeleton

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ import Cart from "./pages/Cart";
 import Congratulation from "./pages/Congratulation";
 import NotFound from "./pages/NotFound";
 
+import Provider from "./helpers/hooks/useGlobalContext";
+
 function App() {
   return (
-    <div className="App">
+    <Provider>
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/categories/:idc" component={Details} />
+          <Route exact path="/categories/:idc" component={Details} />
+          <Route path="/categories/:idc/products/:idp" component={Details} />
           <Route path="/cart" component={Cart} />
           <Route path="/congratulation" component={Congratulation} />
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
-    </div>
+    </Provider>
   );
 }
 
